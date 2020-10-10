b5d37603f56be7dfa3a45aab5d11df6fd47fc0fb
nthu <nthuchatbot@gmail.com>
2020-10-10 23:40:06 +0800

    First commit

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,12 +3,27 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    lines = []
+    tsv.each_line { |line| lines << line.chomp }
+    keys = lines[0].split("\t")
+    @data = (1..lines.length - 1).map do |num|
+      values = lines[num].split("\t")
+      keys.zip(values).to_h
+    end
   end
-
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
-  def to_tsv
 
+  def to_tsv
+    string = ''
+    string <<  'date	student_id	languages	'
+    string <<  "best_language	app_experience	tech_experience\n"
+    @data.each do |hash|
+      hash.each_with_index do |(_, value), idx|
+        string << (idx == (hash.length - 1) ? value.to_s : value.to_s + "\t")
+      end
+      string << "\n"
+    end
+    string
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,17 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  require 'yaml'
+  # take_ymal: converts a String with YMAL data into @data
+  # parameter: ymal - a String in YMAL format
+  def take_yaml(yaml)
+    @data = YAML.safe_load(yaml)
+  end
+
+  # to_ymal: converts @data into ymal string
+  # returns: String in YMAL format
+  def to_yaml
+    @data.to_yaml
+  end
+end
